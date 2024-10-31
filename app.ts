ed22b52ff18a88694131190c00ada12a40501220
Magic Turtle <vacekto@gmail.com>
2024-10-31 22:13:06 +0100

    added express app

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
-Deno.serve((_req) => {
-    return new Response("Hello");
+import express, {Request, Response} from "npm:express@4.18.2";
+
+const app = express();
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.listen(() => {
+    console.log("server started")
 });
